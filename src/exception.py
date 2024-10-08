3608833ccdb665f05b5bee1da67e4fc5b6a26027
risarora <risarora@gmail.com>
2024-10-08 12:55:51 +0530

    Completed Exception and Logging

--- src/exception.py
+++ src/exception.py
@@ -1,22 +1,28 @@
 import sys
+from src.logger import logging
 
 
-def  error_message_details(error,error_details:sys):
-    _,_,exc_tb = error_details.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    line_number=exc_tb.tb_lineno
-    error_message = "Error occured in python script name[{0}] line number[{1}] and error message [{2}]".format(
-     file_name,line_number,str(error)
-    )
+def error_message_details(error, exc_tb):
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = f"Error occurred in Python script {file_name} at line {line_number}: {str(error)}"
     return error_message
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_details:sys):
-        super.__init__(error_message)
-        self.error_message=error_message_details(error_message,error_details=error_details)
+    def __init__(self, error_message):
+        super().__init__(error_message)
+        self.error_message = error_message
 
     def __str__(self):
         return self.error_message
 
+def main():
+    try:
+        a = 1/0
+    except Exception as e:
+        exc_tb = sys.exc_info()[2]
+        logging.error(error_message_details(e, exc_tb))
+        raise CustomException(error_message_details(e, exc_tb))
 
-        
+if __name__ == "__main__":
+    main()
--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,8 @@ from datetime import datetime
 
 
 LOG_File=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_File)
+LOG_Folder=f"{datetime.now().strftime('%m_%d_%Y')}"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_Folder)
 os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_File)
